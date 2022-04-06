bcee91d814482925747b7a88abcd18c473f7720b
“MichaelWen <michaelwen.dev@gmail.com>
2022-04-06 18:02:52 +0100

    feat: created an nft clone

---
+++ contracts/MyNft.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721, Ownable, ERC721Enumerable, ERC721URIStorage{
+     using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+	constructor() ERC721("Coolest NFT", "CNFT") {}
+
+    uint _tokenId = 0;
+
+	function mint(string memory uri) external returns (uint) {
+        _tokenId++;
+		_mint(msg.sender, _tokenId);
+        _setTokenURI(_tokenId, uri);
+		return _tokenId;
+	}
+
+    function getTokenId() external view returns (uint){
+        return _tokenId;
+    }
+
+     // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/nft-test.js
@@ -0,0 +1,47 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MyNftMarket", function () {});
+
+describe("MyNFT", function () {
+  this.timeout(50000);
+
+  let myNFT;
+  let owner;
+  let acc1;
+  let acc2;
+  let market;
+
+  it("Should create and execute market sales", async function () {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+    [owner, acc1, acc2] = await ethers.getSigners();
+
+    const MyNftMarket = await ethers.getContractFactory("MyNftMarket");
+    market = await MyNftMarket.deploy();
+    await market.deployed();
+
+    myNFT = await MyNFT.deploy();
+    await myNFT.deployed();
+
+    const auctionPrice = ethers.utils.parseUnits("10", "ether");
+
+    const nftContractAddress = myNFT.address;
+    const transaction = await myNFT.mint("https://example.com/1");
+    await myNFT.approve(market.address, 1);
+    await market.listToken(nftContractAddress, 1, auctionPrice);
+    const tx = await transaction.wait();
+    console.log(tx.events[0].args[2]);
+
+    await market.buyToken(0, { value: auctionPrice });
+
+    const listingLength = await market.getListingLength();
+
+    for (let index = 0; index < listingLength; index++) {
+      const listing = await market.getListing(index);
+      const [seller, owner, token, tokenId, price, sold] = listing;
+      // console.log(seller, owner, token, tokenId);
+      const tokenUri = await myNFT.tokenURI(tokenId);
+      console.log(tokenUri);
+    }
+  });
+});
